511fdb90f1dcc85922a04d16a3a691a759698cbb
Jesus Zeno <55357012+jesuszeno2@users.noreply.github.com>
2022-05-10 13:23:56 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -2,10 +2,6 @@
 """ 
 Jesus Zeno: I have added another function to allow the data to be viewed in the update tab
 """
-__author__      = "Silvia Nittel"
-__copyright__   = "Copyright 2022, SIE508, University of Maine"
-__credits__     = ["Silvia Nittel"]
-
 
 import mvc_exceptions as mvc_exc
 import basic_backend
