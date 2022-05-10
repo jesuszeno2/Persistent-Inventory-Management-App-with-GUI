1bc1e56b35c53156f4a0bbdf72dc5521d6d68246
Jesus Zeno <55357012+jesuszeno2@users.noreply.github.com>
2022-05-10 13:24:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-"""main.py: main program that uses the inventory items management app"""
-
-__author__      = "Silvia Nittel"
-__copyright__   = "Copyright 2022, SIE508, University of Maine"
-__credits__     = ["Silvia Nittel"]
-
+"""main program that uses the inventory items management app"""
 
 import mvc_exceptions as mvc_exc
 import basic_backend
